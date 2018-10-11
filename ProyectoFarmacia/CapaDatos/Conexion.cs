65ff6d8e036f46fad3378751b11295812de53897
RaimonKoudsi <ramonkoudsi@gmail.com>
2018-10-10 22:44:07 -0400

    Conexion y mitad de Programacion Almacenada

---
+++ ProyectoFarmacia/CapaDatos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string CadenaConexion = "Data Source= DESKTOP-8BRANNO\\SQLEXPRESS; Initial Catalog= ProyectoFarmacia; Integrated Security= true";
+
+    }
+}
--- ProyectoFarmacia/CapaDatos/Properties/Settings.Designer.cs
+++ ProyectoFarmacia/CapaDatos/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProyectoFarmacia.Properties {
+namespace CapaDatos.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
@@ -20,18 +20,6 @@ namespace ProyectoFarmacia.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string ñ {
-            get {
-                return ((string)(this["ñ"]));
-            }
-            set {
-                this["ñ"] = value;
             }
         }
     }
