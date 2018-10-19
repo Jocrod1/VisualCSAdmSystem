eaba5f37055dcde1442f55f0d76d39d8803f8e6c
Mirla M <mirmon.exe@outlook.com>
2018-10-19 00:05:12 -0400

    reporte

--- ProyectoFarmacia/CapaPresentacion/Properties/Settings.Designer.cs
+++ ProyectoFarmacia/CapaPresentacion/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace CapaPresentacion.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MIRLU-PC\\SQLEXPRESS;Initial Catalog=ProyectoFarmacia;Integrated Secur" +
+            "ity=True")]
+        public string ProyectoFarmaciaConnectionString {
+            get {
+                return ((string)(this["ProyectoFarmaciaConnectionString"]));
+            }
+        }
     }
 }
